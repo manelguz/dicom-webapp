efd82f4cd4f9c57409bf05f26e3a7e956b4f0a51
manelguz <manelguz7@gmail.com>
2022-04-16 16:34:03 +0200

    change from image to file base64 response

--- api/api/views.py
+++ api/api/views.py
@@ -1,28 +1,24 @@
 
-from urllib import response
-from wsgiref.util import FileWrapper
-from api.custom_render import JPEGRenderer, PNGRenderer
 from rest_framework import generics
 from images.models import Images    
-from rest_framework.response import Response
-from rest_framework.parsers import FileUploadParser, FormParser, MultiPartParser
+from rest_framework.parsers import FormParser, MultiPartParser
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.http import JsonResponse, HttpResponseNotFound
-from django.core.exceptions import BadRequest
-
+import base64
 
 
 class ImageAPIView(generics.RetrieveAPIView):
 
     parser_classes = [MultiPartParser, FormParser]
-    renderer_classes = [JPEGRenderer]
 
     def get(self, request, *args, **kwargs):
         try:
-            data = Images.objects.get(name=self.kwargs['id']).image
+            data = Images.objects.get(name=self.kwargs['id']).file_dicom
         except Images.DoesNotExist:
             return HttpResponseNotFound("Image does not exist")
-        return Response(data, content_type='image/jpg')
+        dicom_img = data.read()
+        dicom_img_encoded = base64.b64encode(dicom_img).decode('utf-8')
+        return JsonResponse({'dicom_img': dicom_img_encoded})
 
     def delete(self, request, *args, **kwargs):
         try:
@@ -30,6 +26,7 @@ class ImageAPIView(generics.RetrieveAPIView):
         except Images.DoesNotExist:
             return HttpResponseNotFound("Image does not exist")
         object_data.image.delete()
+        object_data.file_dicom.delete()
         object_data.delete()
         return HttpResponse("Image Correctly deleted", content_type="text/plain")
 
@@ -38,13 +35,12 @@ class ImageAPIView(generics.RetrieveAPIView):
         name = request.data['name']
         if Images.objects.filter(name=name).exists():
             return HttpResponseBadRequest('The image already exist with this id.')
-        Images.objects.create(image=file, name=name)
+        Images.objects.create(image=file, name=name, file_dicom=file)
         return HttpResponse("Image Correctly updated", content_type="text/plain")
 
 class ImageAPIList(generics.RetrieveAPIView):
 
     parser_classes = [MultiPartParser, FormParser]
-    renderer_classes = [JPEGRenderer]
 
     def get(self, request, *args, **kwargs):
         data = Images.objects.all()
