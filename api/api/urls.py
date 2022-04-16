93c0fdd946f9ef4e335f02cba375df921b63d346
manelguz <manelguz7@gmail.com>
2022-04-16 16:41:50 +0200

    add code documentation

--- api/api/urls.py
+++ api/api/urls.py
@@ -3,6 +3,6 @@ from .views import ImageAPIView, ImageAPIList
 
 urlpatterns = [
     path('', ImageAPIView.as_view()),
-    path('id/<id>', ImageAPIView.as_view()),
-    path('list', ImageAPIList.as_view()),
+    path('id/<id>', ImageAPIView.as_view()), # get/delete images with the given id
+    path('list', ImageAPIList.as_view()), # path to list the images
 ]
--- api/api/views.py
+++ api/api/views.py
@@ -12,6 +12,9 @@ class ImageAPIView(generics.RetrieveAPIView):
     parser_classes = [MultiPartParser, FormParser]
 
     def get(self, request, *args, **kwargs):
+        """
+        Method to retrive the image from the file form and return to throw the get api response'
+        """
         try:
             data = Images.objects.get(name=self.kwargs['id']).file_dicom
         except Images.DoesNotExist:
@@ -21,6 +24,9 @@ class ImageAPIView(generics.RetrieveAPIView):
         return JsonResponse({'dicom_img': dicom_img_encoded})
 
     def delete(self, request, *args, **kwargs):
+        """
+        Method to delete the image from the db and from the storage system'
+        """
         try:
             object_data = Images.objects.get(name=self.kwargs['id'])
         except Images.DoesNotExist:
@@ -31,6 +37,11 @@ class ImageAPIView(generics.RetrieveAPIView):
         return HttpResponse("Image Correctly deleted", content_type="text/plain")
 
     def post(self, request):
+        """
+        Method to save a new file to the database and the storage service. 
+        The file is saved based on the name provided by the query.
+        If the image alredy exist, return a 400 status code indicating the error
+        """
         file = request.data['file']
         name = request.data['name']
         if Images.objects.filter(name=name).exists():
@@ -43,6 +54,9 @@ class ImageAPIList(generics.RetrieveAPIView):
     parser_classes = [MultiPartParser, FormParser]
 
     def get(self, request, *args, **kwargs):
+        """
+        Method to list all the images in the db.
+        """
         data = Images.objects.all()
         image_names = [img.name for img in data]
         answ = {"all_images": image_names}
