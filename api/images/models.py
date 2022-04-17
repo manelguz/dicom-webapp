1007f99837f7859d5b262350fb5b565008832967
manelguz <manelguz7@gmail.com>
2022-04-17 19:28:11 +0200

    fix minor stuff name functions...

--- api/images/models.py
+++ api/images/models.py
@@ -2,20 +2,25 @@ from django.db import models
 from django.utils import timezone
 import os
 
-def user_directory_path(instance, filename):
+def user_directory_path2(instance, filename):
     if "." in filename:
         filename = filename.split('.')[0]
     return os.path.join('images/', filename + '.jpg')
 
-def user_directory_path2(instance, filename):
+def user_directory_path(instance, filename):
     if "." in filename:
         filename = filename.split('.')[0]
     return os.path.join('images/', filename + '.dcm' )
 
 class Images(models.Model):
-
+    """
+    Object that defines the structue of data that stores the dicom image
+    name: Image name provided by the user
+    file_dicom: The file upload the by the user
+    created: File to register the time when the image was uploaded
+    """
     name =models.CharField(max_length=100)
-    file_dicom = models.FileField(upload_to=user_directory_path2, default='images/default.dcm')
+    file_dicom = models.FileField(upload_to=user_directory_path, default='images/default.dcm')
     image = models.ImageField(
-        upload_to=user_directory_path, default='images/default.jpg')
+        upload_to=user_directory_path2, default='images/default.jpg')
     created = models.DateTimeField(default=timezone.now)
